43cfe9783195688e429fbab5bda00e8106b6f0b9
Martin Beroiz <martinberoiz@gmail.com>
2025-11-20 12:03:08 -0800

    Simple trie example

---
+++ autocomplete/trie.py
@@ -0,0 +1,35 @@
+class Trie:
+
+    def __init__(self, key_value_pairs):
+        self.root = TrieNode()
+        for key, value in key_value_pairs:
+            self.root.insert(key, value)
+
+    def as_dict(self):
+        return self.root.as_dict()
+
+
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.value = None
+        pass
+
+    def insert(self, key, value):
+        if len(key) == 0:
+            self.value = value
+            return self
+        prefix, suffix = key[0], key[1:]
+        if prefix in self.children:
+            child = self.children[prefix]
+            child.insert(suffix, value)
+            return
+
+        # If there is no children with that key, create one
+        child = TrieNode()
+        self.children[prefix] = child
+        child.insert(suffix, value)
+
+    def as_dict(self):
+        return {k: v.as_dict() for k, v in self.children.items()}
